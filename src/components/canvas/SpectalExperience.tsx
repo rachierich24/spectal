872112173210af6c58b549aa694a062b0623422c
rachit vij <rachitvij@rachits-Laptop.local>
2026-06-11 18:16:47 +0530

    feat: remove 3D particles from hero section background, set brand logo as website favicon

--- src/components/canvas/SpectalExperience.tsx
+++ src/components/canvas/SpectalExperience.tsx
@@ -1,22 +1,14 @@
 "use client";
 
-import ArrivalStar from "./ArrivalStar";
 import EnergyWaves from "./EnergyWaves";
 import ConstellationGalaxy from "./ConstellationGalaxy";
 import LegacyPortals from "./LegacyPortals";
 import VIPMode from "./VIPMode";
-import NeuralPlexus from "./NeuralPlexus";
 
 export default function SpectalExperience() {
   return (
     <>
       <VIPMode />
-      
-      {/* Section 1: Neural network plexus environment */}
-      <NeuralPlexus />
-      
-      {/* Section 1: Arrival Star (logo assembly) */}
-      <ArrivalStar />
       
       {/* Section 3: Energy Waves */}
       <EnergyWaves />
